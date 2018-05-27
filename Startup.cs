f7dd212e017fa8813d7278cd0ed32de086d0332f
iamsss <saurav9760@gmail.com>
2018-05-27 21:25:31 +0530

    Creating VegaDbContext

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using vega.Persistence;
 
 namespace vega
 {
@@ -18,6 +20,7 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer("..")); // Registry as Dependency
             services.AddMvc();
 
             // In production, the Angular files will be served from this directory
