5172c7b63555106b6ad39020f85d85837c8ca88f
iamsss <saurav9760@gmail.com>
2018-05-27 23:41:23 +0530

    Apply DataAnnotations Constrains

--- Models/Model.cs
+++ Models/Model.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace vega.Models
 {
     public class Model
     {
          public int Id { get; set; }
+         [Required]
+         [StringLength(255)]
          public string Name { get; set; } 
          
 
