d6aeb9484b1b137124ab9c78624536c949ab8605
iamsss <saurav9760@gmail.com>
2018-05-28 14:23:30 +0530

    Creating Make Service

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MakeService } from './services/make.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -31,7 +32,7 @@ import { VechileFormComponent } from './vechile-form/vechile-form.component';
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
-  providers: [],
+  providers: [MakeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
