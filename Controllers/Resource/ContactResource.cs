a306ed3f96e5fb2a6d55e0cf399acb36623217b5
iamsss <saurav9760@gmail.com>
2018-05-28 19:22:12 +0530

    Adding VehicleResource

---
+++ Controllers/Resource/ContactResource.cs
@@ -0,0 +1,9 @@
+namespace vega.Controllers.Resource
+{
+    public class ContactResource
+    {
+       public string ContactName { get; set; }
+       public string ContactEmail { get; set; }
+       public string ContactPhone { get; set; }
+    }
+}
---
+++ Controllers/Resource/VehicleResource.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using vega.Models;
+
+namespace vega.Controllers.Resource
+{
+
+    public class VehicleResource
+    {
+       public int Id { get; set; }
+       public int ModelId { get; set; }
+       public bool IsRegistered { get; set; }
+
+       public ContactResource contact { get; set; }
+      
+        public ICollection<int> Features { get; set; }
+
+        public VehicleResource()
+        {
+            Features = new Collection<int>();
+        }
+    }
+} 
