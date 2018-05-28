9e2821834110b9b680aacdccb161e37631fe08d9
iamsss <saurav9760@gmail.com>
2018-05-28 18:10:46 +0530

    Adding Vehicle

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -14,5 +14,12 @@ namespace vega.Persistence
         public DbSet<Make> Makes { get; set; }
         public DbSet<Model> Models { get; set; }
         public DbSet<Feature> Features { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf => 
+              new { vf.VehicleId, vf.FeatureId }); // This Pecify that Key To this property has these two keys
+        }
+    
     }
 }
