e96a71f8c2b3892e69ae667c9b9a915dde44b568
iamsss <saurav9760@gmail.com>
2018-05-30 23:20:38 +0530

    Binding Data of vehicle and Sending

--- ClientApp/src/app/services/vehicle.service.ts
+++ ClientApp/src/app/services/vehicle.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError, retry } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
 
 
 @Injectable()
 export class VehicleService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'my-auth-token'
+    })
+  };
 
   constructor(private http: HttpClient) { }
 
@@ -14,4 +22,11 @@ export class VehicleService {
   getFeatures(){
     return this.http.get('/api/features');
   }
+  
+  create(vehicle) {
+        console.log('In Create Vehicle',vehicle);
+        return this.http.post('/api/vehicles', vehicle,this.httpOptions );
+      }
+
+      
 }
